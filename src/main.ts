f5f6c4d96311d7c37c1d232e5f9b3624983e52c7
Nguyen Huy Long <nguyenhuylong.dev@gmail.com>
2026-03-18 22:53:48 +0700

    init

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import helmet from "helmet";
+import "dotenv/config";
+import { ValidationPipe } from "@nestjs/common";
+
+async function bootstrap() {
+  const PORT = process.env.PORT;
+  const ENV = process.env["NODE_ENV"];
+  const URLs = process.env["ACCEPT_URL"] || "";
+
+  if (!PORT) throw new Error("PORT is undefined");
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api/v1");
+  app.use(helmet());
+  app.enableCors({
+    origin:
+      !ENV || ENV === "development" ? "http://localhost:3000" : URLs.split(","),
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(PORT);
+}
+bootstrap().catch((err) => {
+  console.log(err);
+  process.exit();
+});
