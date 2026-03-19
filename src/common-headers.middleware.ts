0d1ead0348ddf5335fb82f211bae9838d6fd443d
Nguyen Huy Long <nguyenhuylong.dev@gmail.com>
2026-03-19 09:32:45 +0700

    fix header

--- src/common-headers.middleware.ts
+++ src/common-headers.middleware.ts
@@ -1,12 +1,12 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
-import { NextFunction, Request, Response } from "express";
+// import { NextFunction, Request, Response } from "express";
 
 @Injectable()
 export class CommonHeadersMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    res.header("X-Powered-By", "LTU-API");
-    res.header("X-System-Status", "Operational");
-    res.header("Content-Type", "application/json; charset=utf-8");
+  use(req: any, res: any, next: () => void) {
+    res.setHeader("X-Powered-By", "LTU-API");
+    res.setHeader("X-System-Status", "Operational");
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
     next();
   }
 }
